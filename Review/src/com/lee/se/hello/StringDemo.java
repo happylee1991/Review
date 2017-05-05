d72a06146bbc81f1572303d0d3e0abb54a1400cc
libin <453940712@qq.com>
2017-05-05 21:23:06 +0800

    test String

---
+++ Review/src/com/lee/se/hello/StringDemo.java
@@ -0,0 +1,12 @@
+package com.lee.se.hello;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str=12345+"";
+		System.out.println(str.substring(0, 2));
+	}
+
+}
