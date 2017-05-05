755b4e285c0f356aa3629eec12fb60a571707c9f
libin <453940712@qq.com>
2017-05-05 21:59:01 +0800

    change DateDemo

--- Review/src/com/lee/se/hello/DateDemo.java
+++ Review/src/com/lee/se/hello/DateDemo.java
@@ -6,7 +6,9 @@ public class DateDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(new Date());
 		System.out.println(new Date().toLocaleString());
+	
 	}
 
 }
