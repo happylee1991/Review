e3ef2387182ab8750976de44dae76ca1c558220d
libin <453940712@qq.com>
2017-05-05 21:55:56 +0800

    add a DateDemo.java file

---
+++ Review/src/com/lee/se/hello/DateDemo.java
@@ -0,0 +1,12 @@
+package com.lee.se.hello;
+
+import java.util.Date;
+
+public class DateDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Date().toLocaleString());
+	}
+
+}
